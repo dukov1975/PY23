f5a4c9ea3d386d0331e93e8165264e7794ad3b2b
dukov1975 <dukov@icloudpocket.ru>
2019-04-15 15:30:18 +0300

    Group PY23

---
+++ 2.2.classes/animals.py
@@ -0,0 +1,139 @@
+class Animal:
+
+    animal_name = ''
+    animal_weight = 0
+
+    def __init__(self, name, weight):
+        self.animal_name = name
+        self.animal_weight = weight
+
+    @property
+    def get_name(self):
+        return self.animal_name
+
+    @property
+    def get_weigth(self):
+        return self.animal_weight
+
+    @property
+    def to_feed(self):
+        return 'Покормить'
+
+class Milk:
+
+    @property
+    def product(self):
+        return 'Подоить'
+
+class Trim:
+
+    @property
+    def product(self):
+        return 'Стрич'
+
+class Egg:
+
+    @property
+    def product(self):
+         return 'Собрать яйца'
+
+
+class Cow(Animal, Milk):
+
+    def __init__(self, name, weight):
+        super().__init__(name, weight)
+
+    @property
+    def voice(self):
+        return 'Му-му'
+
+    @property
+    def type_of(self):
+        return 'Корова'
+
+class Goose(Animal, Egg):
+
+    def __init__(self, name, weight):
+        super().__init__(name, weight)
+
+    @property
+    def voice(self):
+        return 'Га-Га'
+
+    @property
+    def type_of(self):
+        return 'Гусь'
+
+class Ship(Animal, Trim):
+
+    def __init__(self, name, weight):
+        super().__init__(name, weight)
+
+    @property
+    def voice(self):
+        return 'Бе-бе'
+
+    @property
+    def type_of(self):
+        return 'Овца'
+
+class Chicken(Animal, Egg):
+
+    def __init__(self, name, weight):
+        super().__init__(name, weight)
+
+    @property
+    def voice(self):
+        return 'Ко-ко'
+
+    @property
+    def type_of(self):
+        return 'Курица'
+
+
+class Goat(Animal, Milk):
+
+    def __init__(self, name, weight):
+        super().__init__(name, weight)
+
+    @property
+    def voice(self):
+        return 'Ме-ме'
+
+    @property
+    def type_of(self):
+        return 'Коза'
+
+class Duck(Animal, Egg):
+
+    def __init__(self, name, weight):
+        super().__init__(name, weight)
+
+    @property
+    def voice(self):
+        return 'Кря-кря'
+
+    @property
+    def type_of(self):
+        return 'Утка'
+
+
+animals = [Cow('Манька', 320),
+           Goose('Серый', 7),
+           Goose('Белый', 8),
+           Ship('Барашек', 30),
+           Ship('Кудрявый', 35),
+           Chicken('Ко-Ко', 2),
+           Chicken('Кукареку', 3.2),
+           Goat('Рог', 26),
+           Goat('Копыто', 29),
+           Duck('Кряква', 9),
+           ]
+
+total_weigth = 0
+
+for animal_item in animals:
+    print('Тип:', animal_item.type_of, ', Имя:', animal_item.get_name,
+          ', Вес:', animal_item.get_weigth, 'кг, Действие:', animal_item.to_feed,
+          ', Итого:', animal_item.product, ', Голос:', animal_item.voice, sep='')
+
