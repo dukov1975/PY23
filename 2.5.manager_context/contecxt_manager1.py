57dc4571db670a9ff3e94a62249698ee4a7a00b1
dukov1975 <dukov@icloudpocket.ru>
2019-04-29 10:58:44 +0300

    PY23

---
+++ 2.5.manager_context/contecxt_manager1.py
@@ -0,0 +1,25 @@
+
+import datetime
+
+class Estimate:
+
+
+    def __init__(self, date_now):
+        self.start_time = date_now
+        print('Старт контекста ...')
+
+    def __enter__(self):
+        j = 0
+        for i in range(1000000):
+            j = i + j
+        self.code_time = datetime.datetime.now()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print('Время запуска кода :', self.start_time)
+        print('Время выполнения кода :', self.code_time)
+        print('Потрачено времени :', self.code_time - self.start_time)
+
+
+with Estimate(datetime.datetime.now()) as est:
+    print('Работа контекста')
+print('Все, кина не будет  - электричество кончилось')
---
+++ 2.5.manager_context/db.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+class DataConn:
+
+    def __init__(self, db_name):
+        self.db_name = db_name
+
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.db_name)
+        return self.conn
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.conn.close()
+
+
+if __name__ == '__main__':
+
+
+    with DataConn('test.db') as conn:
+        cursor = conn.cursor()
