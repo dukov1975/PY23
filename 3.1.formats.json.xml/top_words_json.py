31aaa6d30c9a70b959f047e2e63beab43f4adce3
dukov1975 <dukov@icloudpocket.ru>
2019-05-01 19:10:19 +0300

    PY23

---
+++ 3.1.formats.json.xml/top_words_json.py
@@ -0,0 +1,22 @@
+import json
+
+with open('newsafr.json') as file:
+    json_dict = json.load(file)
+    news = json_dict['rss']['channel']['items']
+    descript = [item['description'].split(' ') for item in news]
+    all_words = []
+    top_words = dict()
+    for item in descript:
+        for word in item:
+            if len(word) > 6:
+                all_words.append(word)
+    for word in all_words:
+        top_words[word] = all_words.count(word)
+    i = 0
+    for key, value in sorted(top_words.items(), key=lambda item: item[1], reverse=True):
+        i += 1
+        if i > 10:
+            break
+        print(f'{i}. {key} - {value}')
+
+
