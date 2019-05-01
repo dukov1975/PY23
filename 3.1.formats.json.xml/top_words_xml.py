86e68a3dc674e33e3692ef980409d2cadb4c8827
dukov1975 <dukov@icloudpocket.ru>
2019-05-01 19:32:31 +0300

    PY23

---
+++ 3.1.formats.json.xml/top_words_xml.py
@@ -0,0 +1,20 @@
+import xml.etree.ElementTree as ET
+
+
+tree = ET.parse('newsafr.xml')
+items = tree.findall('channel/item')
+all_words = []
+top_words = dict()
+for item in items:
+    description = item.find('description')
+    for word in description.text.split(' '):
+        if len(word) > 6:
+            all_words.append(word)
+for word in all_words:
+    top_words[word] = all_words.count(word)
+i = 0
+for key, value in sorted(top_words.items(), key=lambda item: item[1], reverse=True):
+    i += 1
+    if i > 10:
+        break
+    print(f'{i}. {key} - {value}')
