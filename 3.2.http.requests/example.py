5a2d9b4a763c7fa0f46965b8b00019aebfe60c86
dukov1975 <dukov@icloudpocket.ru>
2019-05-02 17:43:05 +0300

    PY23

--- 3.2.http.requests/example.py
+++ 3.2.http.requests/example.py
@@ -1,6 +1,8 @@
 import requests
+import os
 
-API_KEY = 'trnsl.1.1.20161025T233221Z.47834a66fd7895d0.a95fd4bfde5c1794fa433453956bd261eae80152'
+# API_KEY = 'trnsl.1.1.20161025T233221Z.47834a66fd7895d0.a95fd4bfde5c1794fa433453956bd261eae80152'
+API_KEY = 'trnsl.1.1.20190502T142635Z.78342a3a8ef9e1da.225624bfb31ad38bba3ad3c8e39a794cf401cf76'
 URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
 
 def translate_it(text, to_lang):
@@ -15,20 +17,35 @@ def translate_it(text, to_lang):
 
     :param to_lang:
     :return:
-    """
 
+    """
     params = {
         'key': API_KEY,
         'text': text,
-        'lang': 'ru-{}'.format(to_lang),
+        'lang': '{}-ru'.format(to_lang),
     }
 
     response = requests.get(URL, params=params)
     json_ = response.json()
+    print(json_)
     return ''.join(json_['text'])
+
+for file_name in os.listdir():
+    if file_name.endswith('.txt'):
+        to_lang = file_name[:2]
+        text_to_write = []
+        with open(file_name) as source_file:
+            for line in source_file:
+                if line not in '\n':
+                    text_to_write.append(translate_it(line, to_lang.lower()))
+        write_file = f'{to_lang}_translate.txt'
+        with open(write_file, 'w+') as write_translate:
+            for write_line in text_to_write:
+                write_translate.write(write_line)
+
 
 
 # print(translate_it('В настоящее время доступна единственная опция — признак включения в ответ автоматически определенного языка переводимого текста. Этому соответствует значение 1 этого параметра.', 'no'))
 
 
-requests.post('http://requestb.in/10vc0zh1', json=dict(a='goo', b='foo'))
+# requests.post('http://requestb.in/10vc0zh1', json=dict(a='goo', b='foo'))
